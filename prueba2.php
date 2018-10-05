def6f9b503583c4732731eb7e82d1ec38d7ba527
USP <USP@C303-25>
2018-10-04 22:39:22 -0500

    chero gonzalez - envio 2

--- prueba2.php
+++ prueba2.php
@@ -2,3 +2,25 @@
 
 //lista de numerosfgujteuj
 
+
+function listadoNumeros()
+{
+	echo "<table>
+		<thead>
+			<tr>
+				<th>Listar Numeros</th>
+			</tr>
+		</thead>
+		<tbody>";
+			for($i=1;$i<=3;$i++)
+			{
+				echo "<tr>";
+					echo "<td>".$i."</td>";
+ 					}
+ 				}
+		echo "</tbody>
+	</table>";
+ listadoNumeros();
+?>
+ </body>
+</html>
